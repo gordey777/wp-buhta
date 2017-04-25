66318ec476ac2e98d89353e8937e799e36cbfb08
Oleg Gordiyenko <oleg.gordey777@gmail.com>
2017-04-26 02:08:49 +0300

    done

--- wordpress/wp-content/themes/wp-buhta/page-about.php
+++ wordpress/wp-content/themes/wp-buhta/page-about.php
@@ -33,84 +33,40 @@
     </div><!-- ./slide-content -->
   </header>
 
-  <section class="second">
-    <div class="container-fluid for-about">
-      <h2>Чем можно заняться на турбазе «песчаная бухта»</h2>
-      <p class="card">
-        Бухта Песчаная – визитная карточка Байкала. Это место никого не оставляет равнодушным, поэтому ежегодно сюда стремится попасть все большее количество туристов, в том числе и иностранных. Это место ценят, прежде всего, за нетронутую природу, дарящую великолепные пейзажи, которые давно стали вдохновением художников.
-      </p>
-      <div class="row">
-        <div class="col-md-4 col-sm-12 col-xs-12">
-          <div class="box-wrap box-wrap-about">
-            <div class="underline about-icon">
-              <img src="img/about/ab-icon-1.png" alt="наши преймущества">
-            </div>
-            <div class="box-text-wrap">
-              <h3>Питание</h3>
-              <p>Самый бюджетный и простой способ сделать ремонт в своей квартире и преобразить её внешний вид. Самый бюджетный и простойспособ сделать ремонт в своей квартире и преобразить её внешний вид.</p>
-            </div>
+
+  <?php if( have_rows('about_do') ): ?>
+    <?php while ( have_rows('about_do') ) : the_row(); ?>
+
+      <section class="second">
+        <div class="container-fluid for-about">
+          <h2><?php the_sub_field('title'); ?></h2>
+          <p class="card"><?php the_sub_field('desc'); ?></p>
+          <div class="row">
+
+            <?php if( have_rows('sub_items') ): ?>
+              <?php while ( have_rows('sub_items') ) : the_row(); ?>
+                <div class="col-md-4 col-sm-12 col-xs-12">
+                  <div class="box-wrapbox-wrap-about">
+                    <div class="underline about-icon">
+                      <img src="<?php the_sub_field('sub_img'); ?>">
+                    </div>
+                    <div class="box-text-wrap">
+                      <h3><?php the_sub_field('sub_title'); ?></h3>
+                      <?php the_sub_field('sub_desc'); ?>
+                    </div>
+                  </div>
+                </div>
+              <?php  endwhile; ?>
+            <?php endif; ?>
+
           </div>
         </div>
-        <div class="col-md-4 col-sm-12 col-xs-12">
-          <div class="box-wrap box-wrap-about">
-            <div class="underline about-icon">
-              <img src="img/about/ab-icon-2.png" alt="теплая вода">
-            </div>
-            <div class="box-text-wrap">
-              <h3>Проживание</h3>
-              <p>Самый бюджетный и простой способ сделать ремонт в своей квартире и преобразить её внешний вид. Самый бюджетный и простойспособ сделать ремонт в своей квартире и преобразить её внешний вид.</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4 col-sm-12 col-xs-12">
-          <div class="box-wrap box-wrap-about">
-            <div class="underline about-icon">
-              <img src="img/about/ab-icon-3.png" alt="Уединение с природой">
-            </div>
-            <div class="box-text-wrap">
-              <h3>Благоустройство</h3>
-              <p>Самый бюджетный и простой способ сделать ремонт в своей квартире и преобразить её внешний вид. Самый бюджетный и простойспособ сделать ремонт в своей квартире и преобразить её внешний вид</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-4 col-sm-12 col-xs-12">
-          <div class="box-wrap box-wrap-about">
-            <div class="underline about-icon">
-              <img src="img/about/ab-icon-4.png" alt="наши преймущества">
-            </div>
-            <div class="box-text-wrap">
-              <h3>Питание</h3>
-              <p>Самый бюджетный и простой способ сделать ремонт в своей квартире и преобразить её внешний вид. Самый бюджетный и простойспособ сделать ремонт в своей квартире и преобразить её внешний вид.</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4 col-sm-12 col-xs-12">
-          <div class="box-wrap box-wrap-about">
-            <div class="underline about-icon">
-              <img src="img/about/ab-icon-5.png" alt="теплая вода">
-            </div>
-            <div class="box-text-wrap">
-              <h3>Проживание</h3>
-              <p>Самый бюджетный и простой способ сделать ремонт в своей квартире и преобразить её внешний вид. Самый бюджетный и простойспособ сделать ремонт в своей квартире и преобразить её внешний вид.</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4 col-sm-12 col-xs-12">
-          <div class="box-wrap box-wrap-about">
-            <div class="underline about-icon">
-              <img src="img/about/ab-icon-6.png" alt="Уединение с природой">
-            </div>
-            <div class="box-text-wrap">
-              <h3>Благоустройство</h3>
-              <p>Самый бюджетный и простой способ сделать ремонт в своей квартире и преобразить её внешний вид. Самый бюджетный и простойспособ сделать ремонт в своей квартире и преобразить её внешний вид</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+      </section>
+
+    <?php  endwhile; ?>
+  <?php endif; ?>
+
+
   <section class="price about-form">
     <div class="price-box container-fluid">
       <div class="about-form-left ">
@@ -128,81 +84,34 @@
       </div>
     </div>
   </section>
-  <section class="third-about">
-    <div class="container-fluid">
-      <h2>Чем мы отличаемся от других мест на байкале?</h2>
-      <div class="row first-row">
-        <div class="col-md-4 col-sm-12 col-xs-12">
-          <div class="box-wrap for-outline about-3sec">
-            <div class="img-wrap">
-              <img src="img/home/territory.jpg" alt="наши преймущества">
+
+  <?php if( have_rows('differences') ): ?>
+    <section class="third-about">
+      <div class="container-fluid">
+        <h2>Чем мы отличаемся от других мест на байкале?</h2>
+
+        <div class="row">
+          <?php while ( have_rows('differences') ) : the_row(); ?>
+
+            <div class="col-md-4 col-sm-12 col-xs-12">
+              <div class="box-wrap for-outline about-3sec">
+                <div class="img-wrap">
+                  <img src="<?php the_sub_field('img'); ?>" alt="наши преймущества">
+                </div>
+                <div class="box-text">
+                  <h3><?php the_sub_field('title'); ?></h3>
+                  <p><?php the_sub_field('description'); ?></p>
+                </div>
+              </div>
             </div>
-            <div class="box-text">
-              <h3>Чистая территория</h3>
-              <p>Администрация базы отдыха тщательно следит за чистотой и порядком на прилегающей территории, проводя регулярные уборки. Вы будете наслаждаться исключительно чистой природой Байкала.</p>
-            </div>
-          </div>
+          <?php  endwhile; ?>
         </div>
-        <div class="col-md-4 col-sm-12 col-xs-12">
-          <div class="box-wrap for-outline about-3sec">
-            <div class="img-wrap">
-              <img src="img/home/water.jpg" alt="теплая вода">
-            </div>
-            <div class="box-text">
-              <h3>Теплая вода</h3>
-              <p>Бухта Песчаная – самое солнечное и теплое место на Байкале. Недаром Песчаную называют не иначе как Байкальская Ривьера. В самые жаркие месяцы температура воды может доходить до отметки +22°С. Бархатный сезон приходится на июль и август.</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4 col-sm-12 col-xs-12">
-          <div class="box-wrap for-outline about-3sec">
-            <div class="img-wrap">
-              <img src="img/home/fire.jpg" alt="Уединение с природой">
-            </div>
-            <div class="box-text">
-              <h3>Атмосфера полного уединения с природой</h3>
-              <p>Бухта Песчаная – шедевр, созданный природой, который позволит забыть о повседневной суете и городской пыли. Даже фото этого места поражают воображение. Здесь нет ни автомобильных, ни железнодорожных путей, что позволяет дышать кристально чистым воздухом, наслаждаться потрясающими пейзажами, уютным пляжем, устланным нежным песком, и прозрачной бирюзовой водой Байкала.</p>
-            </div>
-          </div>
-        </div>
+
       </div>
-      <div class="row">
-        <div class="col-md-4 col-sm-12 col-xs-12">
-          <div class="box-wrap for-outline about-3sec">
-            <div class="img-wrap">
-              <img src="img/home/fog.jpg" alt="наши преймущества">
-            </div>
-            <div class="box-text">
-              <h3>Нетронутая природа заповедника</h3>
-              <p>Бухта Песчаная находится на территории национального байкальского парка и признан а важным памятником природы. Именно здесь, на заповедных территориях, можно во всей красе увидеть настоящую дикую природу и посетить уникальные природные объекты.</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4 col-sm-12 col-xs-12">
-          <div class="box-wrap for-outline about-3sec">
-            <div class="img-wrap">
-              <img src="img/home/bird.jpg" alt="теплая вода">
-            </div>
-            <div class="box-text">
-              <h3>Уникальные животные и птицы</h3>
-              <p>На территории заповедника можно встретить множество редких видов животных и птиц, в их числе бакланы, нерпы, лоси, олени, и т.д. В близи от бухты, есть уникальная возможность понаблюдать за охотой бакланов, славящихся своим рыболовным талантом.</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4 col-sm-12 col-xs-12">
-          <div class="box-wrap for-outline about-3sec">
-            <div class="img-wrap">
-              <img src="img/home/rock.jpg" alt="Уединение с природой">
-            </div>
-            <div class="box-text">
-              <h3>Экскурси</h3>
-              <p>Туристическая база «Бухта Песчаная» предлагает посетить 3 бесплатные экскурсии в самые уникальные и труднодоступные места. Масса незабываемых эмоций вам гарантирована.</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+    </section>
+  <?php endif; ?>
+
+
   <section class="selection">
     <div class="container-fluid select">
       <div class="row">
